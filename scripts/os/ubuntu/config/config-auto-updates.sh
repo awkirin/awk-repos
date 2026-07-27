a8191dc9009567a4251babc319bfaeffc584131e
awkirin <awkirin@gmail.com>
2026-07-27 15:32:15 +0300

    Update config-auto-updates.sh

--- scripts/os/ubuntu/config/config-auto-updates.sh
+++ scripts/os/ubuntu/config/config-auto-updates.sh
@@ -1,7 +1,10 @@
-sudo apt update
-sudo apt install -y unattended-upgrades distro-info-data powermgmt-base
+#!/usr/bin/env bash
+set -e
 
-sudo tee /etc/apt/apt.conf.d/1000awkirin-unattended-upgrades > /dev/null <<EOF
+sudo apt update
+sudo apt install -y unattended-upgrades
+
+sudo tee /etc/apt/apt.conf.d/1000-awkirin-unattended-upgrades > /dev/null <<EOF
 
 # Разрешить APT принимать изменения в информации о релизе (полезно при обновлениях версии Ubuntu)
 Acquire::AllowReleaseInfoChanges "true";
@@ -12,12 +15,16 @@ APT::Periodic::Update-Package-Lists "1";
 # Ежедневно запускать unattended-upgrades
 APT::Periodic::Unattended-Upgrade "1";
 
+# Раз в 7 дней удалять устаревшие пакеты из кэша APT
+APT::Periodic::AutocleanInterval "7";
+
 # Автоматически перезагружать сервер, если требуется после обновлений
 Unattended-Upgrade::Automatic-Reboot "true";
 
 # Перезагружать даже при наличии активных пользовательских сессий
-Unattended-Upgrade::Automatic-Reboot-WithUsers "true";
+Unattended-Upgrade::Automatic-Reboot-WithUsers "false";
 
+# Время автоматической перезагрузки
 Unattended-Upgrade::Automatic-Reboot-Time "04:00";
 
 # Автоматически удалять старые неиспользуемые ядра
@@ -28,6 +35,8 @@ Unattended-Upgrade::Remove-New-Unused-Dependencies "true";
 
 EOF
 
-# Включаем и запускаем системные таймеры для ежедневных обновлений APT
-sudo systemctl enable apt-daily.timer apt-daily-upgrade.timer
-sudo systemctl start apt-daily.timer apt-daily-upgrade.timer
+# Включить и запустить системные таймеры APT
+sudo systemctl enable --now apt-daily.timer apt-daily-upgrade.timer
+
+# Проверить корректность конфигурации
+sudo unattended-upgrade --dry-run --debug
